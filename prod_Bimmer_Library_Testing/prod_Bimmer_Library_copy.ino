87d331047b2c7dd2ef037e3085b91b22d2715737
prodnix <cssprod@gmail.com>
2018-09-01 11:58:35 +0100

    Test2

--- prod_Bimmer_Library_Testing/prod_Bimmer_Library_copy.ino
+++ prod_Bimmer_Library_Testing/prod_Bimmer_Library_copy.ino
@@ -5,7 +5,7 @@
 #define debugSerial Serial
 #define btSerial    Serial1
 #define ibusSerial  Serial2
-//#define testSerial  Serial3
+#define testSerial  Serial3
 
 const byte P1_5 = 11;
 const byte P3_6 = 6;
@@ -16,7 +16,7 @@ const byte MFBPin = 8;
 const byte ledPin = 13;
 
 const byte POWER_BUTTON_PRESS [7] = { 0xAA , 0x00 , 0x03 , 0x02 , 0x00 , 0x51 , 0xAA };
-  
+
 const byte POWER_BUTTON_RELEASE [7] = { 0xAA , 0x00 , 0x03 , 0x02 , 0x00 , 0x52 , 0xA9 };
 
 bm64 BM64;
@@ -26,11 +26,11 @@ ibus Ibus;
 //########################################################################
 
 void setup() {
-  
+
   debugSerial.begin(9600);
   btSerial.begin(115200);
   ibusSerial.begin(9600, SERIAL_8E1);
-  
+
   pinMode(btResetPin, OUTPUT);
   pinMode(P1_5, OUTPUT);
   pinMode(P3_6, OUTPUT);
@@ -38,7 +38,7 @@ void setup() {
   pinMode(P2_0Pin, OUTPUT);
   pinMode(EANPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
-  
+
   digitalWrite(btResetPin, HIGH);
   digitalWrite(MFBPin, HIGH);
   digitalWrite(P2_0Pin, HIGH);
@@ -46,19 +46,19 @@ void setup() {
   digitalWrite(P1_5, 1);
   digitalWrite(P3_6, 0);
   digitalWrite(ledPin, 0);
-  
+
   delay(100);
-  
+
   btSerial.write(POWER_BUTTON_PRESS, 7);
   delay(250);
   btSerial.write(POWER_BUTTON_RELEASE, 7);
-  
+
   debugSerial.println("prods BMW Interface Started.");
   delay(1000);
   while (ibusSerial.available()) {
     byte i = ibusSerial.read();
   }
-  
+
 }
 
 //########################################################################
