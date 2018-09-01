97c716a9c970f0154dfe9301639e9dd8f4ce73c7
prodnix <cssprod@gmail.com>
2018-09-01 16:48:47 +0100

    Circular Dump

--- prod_Bimmer_Library/circularBuffer.cpp
+++ prod_Bimmer_Library/circularBuffer.cpp
@@ -5,25 +5,23 @@
 
 circularBuffer::circularBuffer(unsigned int size) {
   size_ = size;
-  buffer_[size_];
+  buffer_[size_] = {0};
   head_ = 0;
   tail_ = 0;
 }
 
 void circularBuffer::In(byte in) {
+  if (head_ == tail_) {
+    head_ = 0;
+    tail_ = 0;
+  }
   buffer_[head_] = in;
   head_ = (head_ + 1) % size_;
 }
 
 void circularBuffer::Out(byte out) {
-  if (tail_ == head_) {
-    debugSerial.println("Buffer Flip!");
-    tail_ = 0;
-    head_ = 0;
-  } else {
     tail_ = (tail_ + out) % size_;
   }
-}
 
 byte circularBuffer::Read(byte index) {
   return(buffer_[(tail_ + index) % size_]);
@@ -36,7 +34,9 @@ byte circularBuffer::Available() {
 
 void circularBuffer::Debug() {
   //byte *ptail_ = tail_;
-  debugSerial.print("tail:");
+  debugSerial.print("Size:");
+  debugSerial.print(size_);
+  debugSerial.print(" tail:");
   debugSerial.print(tail_);
   //debugSerial.print("@");
   //debugSerial.print(&tail_, DEC);
@@ -45,11 +45,24 @@ void circularBuffer::Debug() {
   //debugSerial.print("@");
   //debugSerial.print(&head_, DEC);
   debugSerial.print(" available:");
-  debugSerial.println(Available());
+  debugSerial.print(Available());
+  debugSerial.print(" : ");
+  for (int i = 0; i < Available(); i++) {
+    debugSerial.print(Read(i), HEX);
+    debugSerial.print(";");
+  }
+  debugSerial.println();
 }
 
 void circularBuffer::Reset() {
   head_ = 0;
   tail_ = 0;
   //debugSerial.println("Buffer Reset!!!");
+}
+
+void circularBuffer::Dump() {
+  for (int i = 0; i < 256; i++) {
+    debugSerial.print(buffer_[i], HEX);
+    debugSerial.print(":");
+  }
 }
--- prod_Bimmer_Library/circularBuffer.h
+++ prod_Bimmer_Library/circularBuffer.h
@@ -12,13 +12,14 @@ class circularBuffer
     byte Available();                       // Returns number of bytes in buffer.
     byte Read(byte index);                  // Returns byte from index relative to tail.
     void Debug();                           // Prints head, tail and available bytes to debug serial.
-    void Reset();                           // Reset head and tail to 0.
+    void Reset();
+    void Dump();
     
   private:
     byte head_;                             // Position of head in buffer array.
     byte tail_;                             // Position of tail in buffer array.
     unsigned int size_;                     // Size of buffer array.
-    byte buffer_[];                         // Contents of buffer.
+     buffer_[];                 // Contents of buffer.
 };
 
 #endif
